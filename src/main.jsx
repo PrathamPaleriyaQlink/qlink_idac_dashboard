edf7d625bcc1142383b3b29660337fab86e7af2d
PrathamPaleriyaQlink <pratham.paleriya@qlink.in>
2025-06-27 18:47:16 +0530

    refactor: remove StrictMode wrapper from main render

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <PrimeReactProvider value={{ripple: true}}>
       <App />
     </PrimeReactProvider>
-  </StrictMode>,
 )
